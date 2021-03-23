c599a6695e87b62e5a8aea2f50ef7dd632784552
Tommy Andersson <klomega@ludd.ltu.se>
2021-03-23 18:22:52 +0100

    Updated with the sensor, it's working but have some issues. Might need to change protocol.

--- nuttali/examples/usb-mouse.rs
+++ nuttali/examples/usb-mouse.rs
@@ -5,6 +5,8 @@
 #![no_main]
 #![no_std]
 
+use embedded_hal::spi::MODE_3;
+
 use stm32f4xx_hal::{
     gpio::Speed,
     gpio::{
@@ -29,6 +31,7 @@ use usbd_hid::{
 };
 
 use panic_rtt_target as _;
+
 use app::{
     pmw3389::{self, Register},
     DwtDelay,
@@ -47,7 +50,7 @@ type PMW3389T = pmw3389::Pmw3389<
     PB12<Output<PushPull>>,
 >;
 
-#[rtic::app(device = stm32f4xx_hal::stm32, peripherals = true)]
+#[rtic::app(device = stm32f4xx_hal::stm32, monotonic = rtic::cyccnt::CYCCNT, peripherals = true)]
 const APP: () = {
     struct Resources {
         btn: PC13<Input<PullDown>>,
@@ -55,6 +58,8 @@ const APP: () = {
         usb_dev: UsbDevice<'static, UsbBus<USB>>,
         left_button: PC5<Input<PullDown>>,
         right_button: PA5<Input<PullDown>>,
+
+        pmw3389: PMW3389T,
     }
 
     #[init]
@@ -64,6 +69,8 @@ const APP: () = {
 
         rtt_init_print!();
         rprintln!("init");
+
+        let mut core = ctx.core;
 
         // Set up the system clock.
         let rcc = ctx.device.RCC.constrain();
@@ -88,6 +95,31 @@ const APP: () = {
         };
         USB_BUS.replace(UsbBus::new(usb, EP_MEMORY));
 
+        let sck = gpiob.pb10.into_alternate_af5();
+        let miso = gpioc.pc2.into_alternate_af5();
+        let mosi = gpioc.pc3.into_alternate_af5();
+        let cs = gpiob.pb12.into_push_pull_output().set_speed(Speed::High);
+
+
+        let spi = Spi::spi2(
+            ctx.device.SPI2,
+            (sck, miso, mosi),
+            MODE_3,
+            stm32f4xx_hal::time::KiloHertz(2000).into(),
+            _clocks,
+        );
+
+
+
+        let mut delay = DwtDelay::new(&mut core.DWT, _clocks);
+        let mut pmw3389 = pmw3389::Pmw3389::new(spi, cs, delay).unwrap();
+
+
+
+        // set in burst mode
+        pmw3389.write_register(Register::MotionBurst, 0x00);        
+
+
         let hid = HIDClass::new(USB_BUS.as_ref().unwrap(), MouseReport::desc(), 1);
         let usb_dev = UsbDeviceBuilder::new(USB_BUS.as_ref().unwrap(), UsbVidPid(0xc410, 0x0000))
             .manufacturer("Nuttali")
@@ -96,13 +128,20 @@ const APP: () = {
             .device_class(0)
             .build();
 
-        init::LateResources { btn, hid, usb_dev, left_button, right_button }
+        init::LateResources { btn, hid, usb_dev, left_button, right_button, pmw3389}
     }
 
-    #[task(binds=OTG_FS, resources = [btn, hid, usb_dev, left_button, right_button])]
+    #[task(binds=OTG_FS, resources = [btn, hid, usb_dev, left_button, right_button,pmw3389])]
     fn on_usb(ctx: on_usb::Context) {
-        static mut COUNTER: u16 = 0;
 
+        static mut OLD_POS_X: i8 = 0;
+        static mut OLD_POS_Y: i8 = 0;
+        
+        static mut COUNTER: u16 = 0;
+        let (x, y) = ctx.resources.pmw3389.read_status().unwrap();
+        let mut posX:i8 = ((x)as i8)/50;
+        let mut posY:i8 = ((y)as i8)/50; 
+        rprintln!("X {}, Y {}", posX, posY);
         // destruct the context
         let (btn, usb_dev, hid, ) = (ctx.resources.btn, ctx.resources.usb_dev, ctx.resources.hid);
 
@@ -120,26 +159,19 @@ const APP: () = {
             
 
 
-            x: match *COUNTER {
-                // reached after 100ms
-                100 => {
-                    rprintln!("10");
-                    0
-                }
-                // reached after 199ms
-                199 => {
-                    rprintln!("-10");
-                    -0
-                }
-                _ => 0,
-            },
-            y: 0,
+            x: posX,
+            y: posY,
 
 
             buttons: temp_button,
             //buttons: btn.is_low().unwrap().into(), // (into takes a bool into an integer)
             wheel: 0,
         };
+
+        *OLD_POS_X = posX;
+
+        *OLD_POS_Y = posY;
+
         // wraps around after 200ms
         *COUNTER = (*COUNTER + 1) % 200;
 
