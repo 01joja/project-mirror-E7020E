e0e78e6a2f9081f1f47d36de6617d51163952a41
Tommy Andersson <klomega@ludd.ltu.se>
2021-03-23 17:01:10 +0100

    Left and right button working. Started integrating the sensor code.

--- nuttali/examples/usb-mouse.rs
+++ nuttali/examples/usb-mouse.rs
@@ -6,10 +6,22 @@
 #![no_std]
 
 use stm32f4xx_hal::{
-    gpio::{gpioc::PC13, Input, PullUp},
+    gpio::Speed,
+    gpio::{
+        gpioa::{
+            PA1, PA2, PA3, PA4, PA5, PA6, PA7},
+        gpiob::{
+            PB10,PB12, PB14, PB15},
+        gpioc::{
+            PC2, PC3, PC4, PC5, PC13}, 
+    Input, Output, PushPull, PullDown, Alternate},
     otg_fs::{UsbBus, UsbBusType, USB},
     prelude::*,
+    rcc::Clocks,
+    spi::Spi,
+    stm32,
 };
+
 use usb_device::{bus::UsbBusAllocator, prelude::*};
 use usbd_hid::{
     descriptor::{generator_prelude::*, MouseReport},
@@ -17,14 +29,32 @@ use usbd_hid::{
 };
 
 use panic_rtt_target as _;
+use app::{
+    pmw3389::{self, Register},
+    DwtDelay,
+};
 use rtt_target::{rprintln, rtt_init_print};
+
+type PMW3389T = pmw3389::Pmw3389<
+    Spi<
+        stm32f4xx_hal::stm32::SPI2,
+        (
+            PB10<Alternate<stm32f4xx_hal::gpio::AF5>>,
+            PC2<Alternate<stm32f4xx_hal::gpio::AF5>>,
+            PC3<Alternate<stm32f4xx_hal::gpio::AF5>>,
+        ),
+    >,
+    PB12<Output<PushPull>>,
+>;
 
 #[rtic::app(device = stm32f4xx_hal::stm32, peripherals = true)]
 const APP: () = {
     struct Resources {
-        btn: PC13<Input<PullUp>>,
+        btn: PC13<Input<PullDown>>,
         hid: HIDClass<'static, UsbBusType>,
         usb_dev: UsbDevice<'static, UsbBus<USB>>,
+        left_button: PC5<Input<PullDown>>,
+        right_button: PA5<Input<PullDown>>,
     }
 
     #[init]
@@ -40,9 +70,15 @@ const APP: () = {
         let _clocks = rcc.cfgr.sysclk(48.mhz()).require_pll48clk().freeze();
 
         let gpioc = ctx.device.GPIOC.split();
-        let btn = gpioc.pc13.into_pull_up_input();
-
+        let gpiob = ctx.device.GPIOB.split();
         let gpioa = ctx.device.GPIOA.split();
+
+        let btn = gpioc.pc13.into_pull_down_input();
+
+        let left_button = gpioc.pc5.into_pull_down_input();
+        let right_button = gpioa.pa5.into_pull_down_input();
+
+        //let gpioa = ctx.device.GPIOA.split();
         let usb = USB {
             usb_global: ctx.device.OTG_FS_GLOBAL,
             usb_device: ctx.device.OTG_FS_DEVICE,
@@ -54,38 +90,54 @@ const APP: () = {
 
         let hid = HIDClass::new(USB_BUS.as_ref().unwrap(), MouseReport::desc(), 1);
         let usb_dev = UsbDeviceBuilder::new(USB_BUS.as_ref().unwrap(), UsbVidPid(0xc410, 0x0000))
-            .manufacturer("E70011E")
+            .manufacturer("Nuttali")
             .product("Mouse")
             .serial_number("1.0")
             .device_class(0)
             .build();
 
-        init::LateResources { btn, hid, usb_dev }
+        init::LateResources { btn, hid, usb_dev, left_button, right_button }
     }
 
-    #[task(binds=OTG_FS, resources = [btn, hid, usb_dev])]
+    #[task(binds=OTG_FS, resources = [btn, hid, usb_dev, left_button, right_button])]
     fn on_usb(ctx: on_usb::Context) {
         static mut COUNTER: u16 = 0;
 
         // destruct the context
-        let (btn, usb_dev, hid) = (ctx.resources.btn, ctx.resources.usb_dev, ctx.resources.hid);
+        let (btn, usb_dev, hid, ) = (ctx.resources.btn, ctx.resources.usb_dev, ctx.resources.hid);
+
+        let mut temp_button: u8 = 0;
+
+        if ctx.resources.left_button.is_low().unwrap() {
+            temp_button = temp_button +1;
+        }
+
+        if ctx.resources.right_button.is_low().unwrap() {
+            temp_button = temp_button +2;
+        }
 
         let report = MouseReport {
+            
+
+
             x: match *COUNTER {
                 // reached after 100ms
                 100 => {
                     rprintln!("10");
-                    10
+                    0
                 }
                 // reached after 199ms
                 199 => {
                     rprintln!("-10");
-                    -10
+                    -0
                 }
                 _ => 0,
             },
             y: 0,
-            buttons: btn.is_low().unwrap().into(), // (into takes a bool into an integer)
+
+
+            buttons: temp_button,
+            //buttons: btn.is_low().unwrap().into(), // (into takes a bool into an integer)
             wheel: 0,
         };
         // wraps around after 200ms
